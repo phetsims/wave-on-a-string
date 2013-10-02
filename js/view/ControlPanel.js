334f4cac57dc5a07cffb54d920b8309a052ef833
vauweb <polecat5@yandex.ru>
2013-10-02 15:39:07 +0400

    fixed #5

--- js/view/ControlPanel.js
+++ js/view/ControlPanel.js
@@ -25,18 +25,18 @@ define( function( require ) {
 
   function ControlPanel( model ) {
     Node.call( this, { scale: 1, renderer: 'svg' } );
-    var typeRadio, endTypeRadio;
+    var typeRadio, endTypeRadio, speedSlow, speedGroup;
     this.addChild( typeRadio = new RadioGroup( {radio: ['manual', 'oscillate', 'pulse'], property: model.modeProperty, x: 5, y: 5} ) );
     this.addChild( new RestartButton( model, {x: typeRadio.right + 10, y: 25} ) );
     this.addChild( new PulseButton( model, {x: 130, y: model.height - 135} ) );
     this.addChild( endTypeRadio = new RadioGroup( {radio: ['fixedEnd', 'looseEnd', 'noEnd'], property: model.typeEndProperty, x: model.width - 100, y: 5} ) );
     endTypeRadio.right = model.width - 5;
-    this.addChild( new Node( {scale: 0.7, x: 210, y: model.height - 120, children: [
-      new AquaRadioButton( model.speedProperty, 0.25, new Text( Strings.speedSlow, {font: new PhetFont( 15 )} ), {radius: 12} ),
-      new AquaRadioButton( model.speedProperty, 1, new Text( Strings.speedNormal, {font: new PhetFont( 15 )} ), {radius: 12, x: 130} )
+    this.addChild( speedGroup = new Node( {scale: 0.7, x: 210, y: model.height - 120, children: [
+      speedSlow = new AquaRadioButton( model.speedProperty, 0.25, new Text( Strings.speedSlow, {font: new PhetFont( 15 )} ), {radius: 12} ),
+      new AquaRadioButton( model.speedProperty, 1, new Text( Strings.speedNormal, {font: new PhetFont( 15 )} ), {radius: 12, x: speedSlow.width + 20} )
     ]} ) );
     this.addChild( new BottomControlPanel( model ) );
-    this.addChild( new PlayPauseButton( model, {x: 360, y: model.height - 145} ) );
+    this.addChild( new PlayPauseButton( model, {x: speedGroup.right + 20, y: model.height - 145} ) );
     this.addChild( new Node( { scale: 0.7, right: model.width - 5, y: 430, children: [new ResetAllButton( function() { model.reset(); } )] } ) );
     this.addChild( new WOASTLine( model ) );
     this.addChild( new WOASTTimer( model ) );
