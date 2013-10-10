844f3188a68a9706ae94d43d8621631dbae7640d
Chris Malley <cmalley@pixelzoom.com>
2013-10-10 13:18:24 -0600

    delete stats-r11.js, PhET no longer uses this

--- js/wave-on-a-string-config.js
+++ js/wave-on-a-string-config.js
@@ -12,15 +12,17 @@ require.config( {
   deps: ['wave-on-a-string-main'],
 
   config: {
-    i18n: {
-      locale: 'en_us'
-    }
-  },
+      i18n: {
+        locale: 'en_us'
+      }
+    },
 
   paths: {
 
     // third-party libs
     text: '../../sherpa/text',
+
+    // PhET plugins
     image: '../../chipper/requirejs-plugins/image',
     audio: '../../chipper/requirejs-plugins/audio',
     string: '../../chipper/requirejs-plugins/string',
@@ -37,19 +39,10 @@ require.config( {
     SCENERY: '../../scenery/js',
     SCENERY_PHET: '../../scenery-phet/js',
     SUN: '../../sun/js',
-    // contrib dependencies required by common directories
-    //REVIEW: if the stats code isn't being used to measure performance, this should be removed
-    stats: '../../phetcommon/contrib/stats-r11',
 
+    // this sim
     WOAS: '.'
   },
-  // Configure the dependencies and exports for older, traditional 'browser globals' scripts
-  // that do not use define() to declare the dependencies and set a module value.
-  //REVIEW: if the stats code isn't being used to measure performance, this should be removed
-  shim: {
-    stats: {
-      exports: 'Stats'
-    }
-  },
+
   urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
 } );
