ad345bd87211938568e8730c5e79758ae4911989
Jonathan Olson <olsonsjc@gmail.com>
2014-09-02 21:06:17 -0600

    Adding pulse icon to pulse button, fixes #85

--- js/view/control/PulseButton.js
+++ js/view/control/PulseButton.js
@@ -10,12 +10,30 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var Shape = require( 'KITE/Shape' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
   var RoundPushButton = require( 'SUN/buttons/RoundPushButton' );
 
   function PulseButton( model, options ) {
+    var pulseShape = new Shape().moveTo( -9, 0 ).lineTo( -4, 0 ).lineTo( 0, -6 ).lineTo( 4, 0 ).lineTo( 9, 0 );
+
+    var pulsePath = new Path( pulseShape, {
+      lineWidth: 1,
+      stroke: '#333',
+      lineCap: 'round'
+    } );
+
+    var pulsePath2 = new Path( pulseShape, {
+      lineWidth: 3,
+      stroke: '#5f5',
+      lineCap: 'round'
+    } );
+
     RoundPushButton.call( this, {
       listener: model.manualPulse.bind( model ),
       baseColor: '#33dd33',
+      content: new Node( { children: [pulsePath2, pulsePath] } ),
       radius: 17
     } );
     var self = this;
