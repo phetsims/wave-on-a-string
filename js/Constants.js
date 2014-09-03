823edda27bc7a24a2f1a0bcbdbfe46f83cd2b424
Jonathan Olson <olsonsjc@gmail.com>
2014-09-03 00:02:01 -0600

    Updating amplitude range to fit in the window

--- js/Constants.js
+++ js/Constants.js
@@ -39,7 +39,7 @@ define( function( require ) {
     dampingRange: new Range( 0, 100 ),
     frequencyRange: new Range( 0, 3 ),
     pulseWidthRange: new Range( 0.2, 1 ),
-    amplitudeRange: new Range( 0, 1.5 ),
+    amplitudeRange: new Range( 0, 1.25 ),
 
     postGradient: new LinearGradient( -5, 0, 5, 0 )
       .addColorStop( 0, '#666' )
