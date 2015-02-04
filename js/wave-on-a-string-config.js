f546e225c75ec0bd57dafa4f71912c7338d33266
Sam Reid <reids@colorado.edu>
2015-02-03 20:52:49 -0700

    Added cache buster as an option & standardized formatting for config files, see https://github.com/phetsims/joist/issues/196

--- js/wave-on-a-string-config.js
+++ js/wave-on-a-string-config.js
@@ -38,5 +38,6 @@ require.config( {
     WAVE_ON_A_STRING: '.'
   },
 
-  urlArgs: new Date().getTime()  // cache buster to make browser refresh load all included scripts
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.phetcommon.getCacheBusterArgs()
 } );
