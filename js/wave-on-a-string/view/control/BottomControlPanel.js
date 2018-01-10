cca2124d321c07b458c685706516d495df556cc9
samreid <reids@colorado.edu>
2018-01-10 11:36:53 -0700

    Minor formatting changes

--- js/wave-on-a-string/view/control/BottomControlPanel.js
+++ js/wave-on-a-string/view/control/BottomControlPanel.js
@@ -46,21 +46,19 @@ define( function( require ) {
       font: new PhetFont( 15 ),
       maxWidth: 130
     };
-    var checkBoxGroup = new VerticalCheckBoxGroup( [
-      {
-        content: new Text( rulersString, checkBoxTextOptions ),
-        property: model.rulersProperty,
-        indent: 0
-      }, {
-        content: new Text( timerString, checkBoxTextOptions ),
-        property: model.timerProperty,
-        indent: 0
-      }, {
-        content: new Text( referenceLineString, checkBoxTextOptions ),
-        property: model.referenceLineProperty,
-        indent: 0
-      }
-    ] );
+    var checkBoxGroup = new VerticalCheckBoxGroup( [ {
+      content: new Text( rulersString, checkBoxTextOptions ),
+      property: model.rulersProperty,
+      indent: 0
+    }, {
+      content: new Text( timerString, checkBoxTextOptions ),
+      property: model.timerProperty,
+      indent: 0
+    }, {
+      content: new Text( referenceLineString, checkBoxTextOptions ),
+      property: model.referenceLineProperty,
+      indent: 0
+    } ] );
 
     var separator = new Line( 0, 10, 0, 100, { stroke: 'gray', lineWidth: 1 } );
 
@@ -74,11 +72,11 @@ define( function( require ) {
       range: Constants.tensionRange,
       titleVerticalOffset: 15,
       tick: { step: 0.25, minText: lowString, maxText: highString },
-      constrainValue: function( value ){
+      constrainValue: function( value ) {
         // logic to round the value to nearest .25 to have snap behaviour
         value = Util.toFixedNumber( value, 2 );
         value = value * 100;
-        value = Util.roundSymmetric( value / 25 ) * 25 ;
+        value = Util.roundSymmetric( value / 25 ) * 25;
         value = value / 100;
         return value;
       }
