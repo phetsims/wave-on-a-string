5c26b8d0f9d98b73cf328d61840da56f9cbdc89b
phet-dev <phetadmin@gmail.com>
2020-02-01 04:04:41 -0700

    update copyright dates from daily grunt work

--- js/wave-on-a-string/model/WOASModelIO.js
+++ js/wave-on-a-string/model/WOASModelIO.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2020, University of Colorado Boulder
+// Copyright 2020, University of Colorado Boulder
 
 /**
  * IO type for the main model, including the typed array information.
