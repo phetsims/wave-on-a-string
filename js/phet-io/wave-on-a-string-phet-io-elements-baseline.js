e217a03093e119f8d9e43568d62b377c208e97e3
zepumph <michael.kauzmann@colorado.edu>
2020-03-16 14:41:54 -0800

    remove ResetAllButtonIO.js, https://github.com/phetsims/scenery-phet/issues/572

--- js/phet-io/wave-on-a-string-phet-io-elements-baseline.js
+++ js/phet-io/wave-on-a-string-phet-io-elements-baseline.js
@@ -6772,7 +6772,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioReadOnly": false,
       "phetioState": false,
       "phetioStudioControl": true,
-      "phetioTypeName": "ResetAllButtonIO"
+      "phetioTypeName": "NodeIO"
     },
     "waveOnAString.waveOnAStringScreen.view.resetAllButton.enabledProperty": {
       "phetioDocumentation": "When disabled, the button is grayed out and cannot be pressed",
