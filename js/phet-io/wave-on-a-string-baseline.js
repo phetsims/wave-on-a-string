3dcc82fa7ece586f0a63993bbe9c34f91b2d3c7b
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:30:30 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/wave-on-a-string-baseline.js
+++ js/phet-io/wave-on-a-string-baseline.js
@@ -1314,7 +1314,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "ActionIO<NullableIO<SceneryEventIO>>"
     },
-    "waveOnAString.general.view.navigationBar.titleTextNode": {
+    "waveOnAString.general.view.navigationBar.titleText": {
       "phetioDocumentation": "Displays the title of the simulation in the navigation bar (bottom left)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1327,7 +1327,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "TextIO"
     },
-    "waveOnAString.general.view.navigationBar.titleTextNode.opacityProperty": {
+    "waveOnAString.general.view.navigationBar.titleText.opacityProperty": {
       "phetioDocumentation": "Opacity of the parent NodeIO, between 0 (invisible) and 1 (fully visible)",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1340,7 +1340,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "NumberPropertyIO"
     },
-    "waveOnAString.general.view.navigationBar.titleTextNode.pickableProperty": {
+    "waveOnAString.general.view.navigationBar.titleText.pickableProperty": {
       "phetioDocumentation": "Sets whether the node will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1353,7 +1353,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<NullableIO<BooleanIO>>"
     },
-    "waveOnAString.general.view.navigationBar.titleTextNode.textProperty": {
+    "waveOnAString.general.view.navigationBar.titleText.textProperty": {
       "phetioDocumentation": "Property for the displayed text",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
@@ -1366,7 +1366,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<StringIO>"
     },
-    "waveOnAString.general.view.navigationBar.titleTextNode.visibleProperty": {
+    "waveOnAString.general.view.navigationBar.titleText.visibleProperty": {
       "phetioDocumentation": "Controls whether the Node will be visible (and interactive), see the NodeIO documentation for more details.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
