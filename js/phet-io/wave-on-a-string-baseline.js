7d34d71a346f8dfe8cf5e19d9b83d7a4cc2009c5
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-15 19:16:28 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/wave-on-a-string-baseline.js
+++ js/phet-io/wave-on-a-string-baseline.js
@@ -1834,19 +1834,6 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "waveOnAString.waveOnAStringScreen.nameProperty": {
-      "phetioDocumentation": "The name of the screen. Changing this value will update the screen name for the screen's corresponding button on the navigation bar and home screen, if they exist. May be null for single-screen simulations.",
-      "phetioDynamicElement": false,
-      "phetioEventType": "MODEL",
-      "phetioFeatured": true,
-      "phetioHighFrequency": false,
-      "phetioIsArchetype": false,
-      "phetioPlayback": false,
-      "phetioReadOnly": false,
-      "phetioState": true,
-      "phetioStudioControl": true,
-      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
-    },
     "waveOnAString.waveOnAStringScreen.view.bottomControlPanel.amplitudeControl": {
       "phetioDocumentation": "",
       "phetioDynamicElement": false,
