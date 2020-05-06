57bb8bfbd20247e6d045e857b790481e7c0f2f94
Jonathan Olson <jonathan.olson@colorado.edu>
2020-05-06 16:25:34 -0600

    Swapping damping control to different style, see https://github.com/phetsims/wave-on-a-string/issues/116

--- js/wave-on-a-string/view/BottomControlPanel.js
+++ js/wave-on-a-string/view/BottomControlPanel.js
@@ -6,26 +6,23 @@
  * @author Anton Ulyanov (Mlearner)
  */
 
-import Utils from '../../../../dot/js/Utils.js';
-import merge from '../../../../phet-core/js/merge.js';
+import DynamicProperty from '../../../../axon/js/DynamicProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import Range from '../../../../dot/js/Range.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import Line from '../../../../scenery/js/nodes/Line.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
-import VBox from '../../../../scenery/js/nodes/VBox.js';
-import HSlider from '../../../../sun/js/HSlider.js';
 import Panel from '../../../../sun/js/Panel.js';
 import VerticalCheckboxGroup from '../../../../sun/js/VerticalCheckboxGroup.js';
-import waveOnAStringStrings from '../../waveOnAStringStrings.js';
 import waveOnAString from '../../waveOnAString.js';
+import waveOnAStringStrings from '../../waveOnAStringStrings.js';
 import WOASModel from '../model/WOASModel.js';
 import WOASNumberControl from './WOASNumberControl.js';
 
 const amplitudeString = waveOnAStringStrings.amplitude;
 const dampingString = waveOnAStringStrings.damping;
 const frequencyString = waveOnAStringStrings.frequency;
-const highString = waveOnAStringStrings.high;
-const lowString = waveOnAStringStrings.low;
 const patternValueUnitCmString = waveOnAStringStrings.patternValueUnitCm;
 const patternValueUnitHzString = waveOnAStringStrings.patternValueUnitHz;
 const patternValueUnitPercentageString = waveOnAStringStrings.patternValueUnitPercentage;
@@ -73,49 +70,22 @@ class BottomControlPanel extends Node {
     separator.right = checkboxGroup.left - 20;
     checkboxGroup.centerY = separator.centerY;
 
-    const tensionControlTandem = tandem.createTandem( 'tensionControl' );
-    const tensionSlider = new HSlider( model.tensionProperty, model.tensionProperty.range, merge( {
-      constrainValue: value => {
-        // logic to round the value to nearest .25 to have snap behaviour
-        value = Utils.toFixedNumber( value, 2 );
-        value = value * 100;
-        value = Utils.roundSymmetric( value / 25 ) * 25;
-        value = value / 100;
-        return value;
+    const tensionProperty = new DynamicProperty( new Property( model.tensionProperty ), {
+      bidirectional: true,
+      map: value => value * 100,
+      inverseMap: value => value / 100
+    } );
+
+    // TODO: how to support range on dynamic properties?
+    tensionProperty.range = new Range( model.tensionProperty.range.min * 100, model.tensionProperty.range.max * 100 );
+
+    const tensionControl = new WOASNumberControl( tensionString, tensionProperty, {
+      delta: 5,
+      numberDisplayOptions: {
+        decimalPlaces: 0,
+        valuePattern: patternValueUnitPercentageString
       },
-
-      tandem: tensionControlTandem.createTandem( 'slider' )
-    }, WOASNumberControl.getSliderOptions() ) );
-
-    // tensionSlider ticks
-    for ( let i = model.tensionProperty.range.min; i <= model.tensionProperty.range.max; i += 0.25 ) {
-      if ( i === model.tensionProperty.range.max ) {
-        tensionSlider.addMajorTick( i, new Text( highString, { font: new PhetFont( 15 ), maxWidth: 40 } ) );
-      }
-      else if ( i === model.tensionProperty.range.min ) {
-        tensionSlider.addMajorTick( i, new Text( lowString, { font: new PhetFont( 15 ), maxWidth: 40 } ) );
-      }
-      else if ( i === ( model.tensionProperty.range.min + model.tensionProperty.range.max ) / 2 ) {
-        tensionSlider.addMajorTick( i );
-      }
-      else {
-        tensionSlider.addMinorTick( i );
-      }
-    }
-
-    // TODO: clean up layout!!! wow
-
-    const tensionControl = new VBox( {
-      spacing: 12,
-      align: 'center',
-      children: [
-        new Text( tensionString, {
-          font: new PhetFont( 18 ),
-          maxWidth: 150
-        } ),
-        tensionSlider
-      ],
-      tandem: tensionControlTandem
+      tandem: tandem.createTandem( 'tensionControl' )
     } );
 
     tensionControl.right = separator.left - 20;
