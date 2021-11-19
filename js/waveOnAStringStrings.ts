3e0553ad92e3362dde6a4932cba6cbcc801352dc
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:44 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/waveOnAStringStrings.ts
@@ -0,0 +1,44 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import waveOnAString from './waveOnAString.js';
+
+type StringsType = {
+  'wave-on-a-string': {
+    'title': string
+  },
+  'rulers': string,
+  'timer': string,
+  'amplitude': string,
+  'frequency': string,
+  'pulseWidth': string,
+  'damping': string,
+  'tension': string,
+  'low': string,
+  'high': string,
+  'manual': string,
+  'oscillate': string,
+  'pulse': string,
+  'restart': string,
+  'fixedEnd': string,
+  'looseEnd': string,
+  'noEnd': string,
+  'speedNormal': string,
+  'speedSlow': string,
+  'referenceLine': string,
+  'unitCm': string,
+  'patternValueUnitHz': string,
+  'patternValueUnitCm': string,
+  'patternValueUnitS': string,
+  'patternValueUnitPercentage': string
+};
+
+const waveOnAStringStrings = getStringModule( 'WAVE_ON_A_STRING' ) as StringsType;
+
+waveOnAString.register( 'waveOnAStringStrings', waveOnAStringStrings );
+
+export default waveOnAStringStrings;
