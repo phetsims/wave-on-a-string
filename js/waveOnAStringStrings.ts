ffec91d212ae6a422782fce506cacc90140008ad
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:38 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/waveOnAStringStrings.ts
+++ js/waveOnAStringStrings.ts
@@ -5,36 +5,62 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import waveOnAString from './waveOnAString.js';
 
 type StringsType = {
   'wave-on-a-string': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'rulers': string;
+  'rulersProperty': TReadOnlyProperty<string>;
   'timer': string;
+  'timerProperty': TReadOnlyProperty<string>;
   'amplitude': string;
+  'amplitudeProperty': TReadOnlyProperty<string>;
   'frequency': string;
+  'frequencyProperty': TReadOnlyProperty<string>;
   'pulseWidth': string;
+  'pulseWidthProperty': TReadOnlyProperty<string>;
   'damping': string;
+  'dampingProperty': TReadOnlyProperty<string>;
   'tension': string;
+  'tensionProperty': TReadOnlyProperty<string>;
   'low': string;
+  'lowProperty': TReadOnlyProperty<string>;
   'high': string;
+  'highProperty': TReadOnlyProperty<string>;
   'manual': string;
+  'manualProperty': TReadOnlyProperty<string>;
   'oscillate': string;
+  'oscillateProperty': TReadOnlyProperty<string>;
   'pulse': string;
+  'pulseProperty': TReadOnlyProperty<string>;
   'restart': string;
+  'restartProperty': TReadOnlyProperty<string>;
   'fixedEnd': string;
+  'fixedEndProperty': TReadOnlyProperty<string>;
   'looseEnd': string;
+  'looseEndProperty': TReadOnlyProperty<string>;
   'noEnd': string;
+  'noEndProperty': TReadOnlyProperty<string>;
   'speedNormal': string;
+  'speedNormalProperty': TReadOnlyProperty<string>;
   'speedSlow': string;
+  'speedSlowProperty': TReadOnlyProperty<string>;
   'referenceLine': string;
+  'referenceLineProperty': TReadOnlyProperty<string>;
   'unitCm': string;
+  'unitCmProperty': TReadOnlyProperty<string>;
   'patternValueUnitHz': string;
+  'patternValueUnitHzProperty': TReadOnlyProperty<string>;
   'patternValueUnitCm': string;
+  'patternValueUnitCmProperty': TReadOnlyProperty<string>;
   'patternValueUnitS': string;
+  'patternValueUnitSProperty': TReadOnlyProperty<string>;
   'patternValueUnitPercentage': string;
+  'patternValueUnitPercentageProperty': TReadOnlyProperty<string>;
 };
 
 const waveOnAStringStrings = getStringModule( 'WAVE_ON_A_STRING' ) as StringsType;
