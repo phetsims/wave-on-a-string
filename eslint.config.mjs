28b645bc43a9813371e479e0b1b56573b3b96dc4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:54 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for wave-on-a-string.
+ * ESLint configuration for wave-on-a-string.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
