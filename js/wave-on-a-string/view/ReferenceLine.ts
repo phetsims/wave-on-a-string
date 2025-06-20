f9c5b8c9a17b4bb962e0d2f40ed8f9b0f4271a9a
Jonathan Olson <jonathan.olson@colorado.edu>
2025-06-20 16:33:38 -0600

    Fix up horizontal reference line, see https://github.com/phetsims/wave-on-a-string/issues/112

--- js/wave-on-a-string/view/ReferenceLine.ts
+++ js/wave-on-a-string/view/ReferenceLine.ts
@@ -8,6 +8,7 @@
 
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
+import ScreenView from '../../../../joist/js/ScreenView.js';
 import Shape from '../../../../kite/js/Shape.js';
 import DragListener from '../../../../scenery/js/listeners/DragListener.js';
 import Line from '../../../../scenery/js/nodes/Line.js';
@@ -26,20 +27,24 @@ export default class ReferenceLine extends Node {
   ) {
     super( { cursor: 'pointer', tandem: tandem } );
 
-    this.addChild( new Rectangle( 740 * 2, -10, 40, 20, {
+    model.referenceLinePositionProperty.link( console.log );
+
+    const referenceX = ScreenView.DEFAULT_LAYOUT_BOUNDS.right - 28;
+
+    this.addChild( new Rectangle( referenceX * 2, -10, 40, 20, {
       fill: referenceLineBlockGradient,
       scale: 0.5,
       stroke: '#000',
       lineWidth: 0.5
     } ) );
-    this.addChild( new Rectangle( 750, -10, 20, 20, {
+    this.addChild( new Rectangle( referenceX + 10, -10, 20, 20, {
       fill: referenceLineBlockGradient,
       stroke: '#000',
       lineWidth: 0.5
     } ) );
-    this.addChild( new Line( 0, 0, 750, 0, {
-      mouseArea: new Bounds2( 0, 0, 750, 0 ).dilated( 5 ),
-      touchArea: new Bounds2( 0, 0, 750, 0 ).dilated( 10 ),
+    this.addChild( new Line( 0, 0, referenceX + 10, 0, {
+      mouseArea: new Bounds2( 0, 0, referenceX + 10, 0 ).dilated( 5 ),
+      touchArea: new Bounds2( 0, 0, referenceX + 10, 0 ).dilated( 10 ),
       stroke: '#F00',
       lineDash: [ 10, 6 ],
       lineWidth: 2
@@ -52,8 +57,8 @@ export default class ReferenceLine extends Node {
       this.translation = position;
     } );
 
-    this.touchArea = Shape.bounds( Bounds2.point( 755, 0 ).dilated( dilatedReferenceLineTouchArea ) );
-    this.mouseArea = Shape.bounds( Bounds2.point( 755, 0 ).dilatedXY( 15, 10 ) );
+    this.touchArea = Shape.bounds( Bounds2.point( referenceX + 15, 0 ).dilated( dilatedReferenceLineTouchArea ) );
+    this.mouseArea = Shape.bounds( Bounds2.point( referenceX + 15, 0 ).dilatedXY( 15, 10 ) );
 
     this.addInputListener( new DragListener( {
       positionProperty: model.referenceLinePositionProperty,
