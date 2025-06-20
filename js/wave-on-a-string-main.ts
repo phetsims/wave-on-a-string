7fcbbc3d851230e183c5e1cc5bf1e35885989ccd
Sam Reid <reids@colorado.edu>
2025-06-20 08:55:54 -0600

    Convert to TypeScript, see https://github.com/phetsims/wave-on-a-string/issues/161

--- js/wave-on-a-string-main.ts
+++ js/wave-on-a-string-main.ts
@@ -14,20 +14,18 @@ import WaveOnAStringStrings from './WaveOnAStringStrings.js';
 
 const waveOnAStringTitleStringProperty = WaveOnAStringStrings[ 'wave-on-a-string' ].titleStringProperty;
 
-const simOptions = {
-  credits: {
-    leadDesign: 'Michael Dubson, Ariel Paul',
-    softwareDevelopment: 'Jonathan Olson, Michael Dubson',
-    team: 'Trish Loeblein, Ariel Paul, Kathy Perkins, Amy Rouinfar',
-    graphicArts: 'Sharon Siman-Tov',
-    thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team ' +
-            'to convert this simulation to HTML5.'
-  }
-};
-
 simLauncher.launch( () => {
   //Create and start the sim
   new Sim( waveOnAStringTitleStringProperty, [
     new WOASScreen( Tandem.ROOT.createTandem( 'waveOnAStringScreen' ) )
-  ], simOptions ).start();
+  ], {
+    credits: {
+      leadDesign: 'Michael Dubson, Ariel Paul',
+      softwareDevelopment: 'Jonathan Olson, Michael Dubson',
+      team: 'Trish Loeblein, Ariel Paul, Kathy Perkins, Amy Rouinfar',
+      graphicArts: 'Sharon Siman-Tov',
+      thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team ' +
+              'to convert this simulation to HTML5.'
+    }
+  } ).start();
 } );
